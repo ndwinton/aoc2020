c7e36efea53e7772ff39963872a4a2c6232c4b8d
Neil Winton <nwinton@pivotal.io>
2020-12-16 14:32:17 +0000

    Day 15 - Part 2, using mutable state, much as it pains me ... :-(

--- src/main/kotlin/advent2020/Day15.kt
+++ src/main/kotlin/advent2020/Day15.kt
@@ -4,35 +4,31 @@ data class StateInfo(val lastIndex: Int, val previousIndex: Int)
 
 val NO_STATE = StateInfo(0, 0)
 
-class GameState(val lastValue: Int, val lastTurn: Int, val stateMap: Map<Int,StateInfo>) {
+class GameState(val lastValue: Int, val lastTurn: Int, private val stateMap: MutableMap<Int,StateInfo>) {
 
-    constructor(stateString: String) : this(lastStartingNumber(stateString), lastTurnNumber(stateString), startingMap(stateString)) {
-        val map = stateString.split(",")
-            .mapIndexed { index, str -> Pair(str.toInt(), Pair(index + 1, index + 1)) }
-            .toMap()
-    }
+    constructor(stateString: String) : this(lastStartingNumber(stateString), lastTurnNumber(stateString), startingMap(stateString))
 
     fun stateFor(number: Int): StateInfo = stateMap.getOrDefault(number, NO_STATE)
 
     fun next(): GameState {
-        if (lastTurn % 1000 == 0) {
-            println("$lastTurn / $lastValue / ${stateMap.size}")
-        }
         val lastDiff = stateFor(lastValue).lastIndex - stateFor(lastValue).previousIndex
         val lastDiffState = stateFor(lastDiff)
         val turn = lastTurn + 1
         return if (lastDiffState == NO_STATE) {
-            GameState(lastDiff, turn, stateMap + Pair(lastDiff, StateInfo(turn, turn)))
+            stateMap[lastDiff] = StateInfo(turn, turn)
+            GameState(lastDiff, turn, stateMap)
         } else {
-            GameState(lastDiff, turn, stateMap + Pair(lastDiff, StateInfo(turn, lastDiffState.lastIndex)))
+            stateMap[lastDiff] = StateInfo(turn, lastDiffState.lastIndex)
+            GameState(lastDiff, turn, stateMap)
         }
     }
 }
 
-private fun startingMap(input: String): Map<Int,StateInfo> =
+private fun startingMap(input: String): MutableMap<Int,StateInfo> =
     input.split(",")
         .mapIndexed { index, str -> Pair(str.toInt(), StateInfo(index + 1, index + 1)) }
         .toMap()
+        .toMutableMap()
 
 private fun lastStartingNumber(input: String): Int = input.split(",").last().toInt()
 
